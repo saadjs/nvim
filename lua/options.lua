6c7d715e32acc1d1f152702404057012b146c582
Saad Bash <saadbash08@gmail.com>
2024-01-01 19:50:13 -0500

    added none-ls for auto-formatting

---
+++ lua/options.lua
@@ -0,0 +1,59 @@
+-- [[ Setting options ]]
+-- `:help vim.opt`
+
+-- Set highlight on search
+vim.opt.hlsearch = false
+
+-- Make line numbers default
+vim.opt.nu = true
+
+-- Turn on relative line numbers
+vim.opt.relativenumber = true
+
+-- relative line numbers in netrw
+vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.smartindent = true
+vim.g.autoformat = true
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.opt.termguicolors = true
+
+-- Confirm before exiting if there are unsaved changes
+vim.opt.confirm = true

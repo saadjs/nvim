b07832da54204843cf5ffbdf664697ac02551370
Saad Bash <saad.bash@cvshealth.com>
2024-01-02 16:04:08 -0500

    moved plugin to plugins dir: telescope

--- init.lua
+++ init.lua
@@ -17,24 +17,6 @@ require("options")
 require("lazy").setup({
   { import = "plugins" },
 
-  -- Useful plugin to show you pending keybinds.
-
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        cond = function()
-          return vim.fn.executable("make") == 1
-        end,
-      },
-    },
-  },
-
   {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
@@ -44,22 +26,6 @@ require("lazy").setup({
     build = ":TSUpdate",
   },
 })
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require("telescope").setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
-      },
-    },
-  },
-})
-
--- Enable telescope fzf native, if installed
-pcall(require("telescope").load_extension, "fzf")
 
 -- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path

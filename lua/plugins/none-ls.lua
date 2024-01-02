11cc34fa9a8dc9fdb8c34c103d347c73ff6faa72
Saad Bash <saad.bash@cvshealth.com>
2024-01-02 15:27:49 -0500

    moved few plugins to plugins dir

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "jay-babu/mason-null-ls.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local mason_null_ls = require("mason-null-ls")
+
+      mason_null_ls.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+          "black", -- python formatter
+          "pylint", -- python linter
+          "eslint_d", -- js linter
+        },
+      })
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.eslint_d,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+}

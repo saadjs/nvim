b36261ad1e5e28921443048b21c5266a607a4ec9
Saad Bash <saad.bash@cvshealth.com>
2024-05-15 09:10:12 -0400

    add new keymaps for split management

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -35,3 +35,18 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- Keep window centered when scrolling up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- Resizing splits
+-- Vertical
+vim.keymap.set("n", "<leader>-", "<C-w>5<", { desc = "Decrease split width"})
+vim.keymap.set("n", "<leader>=", "<C-w>5>", { desc = "Increase split width"})
+
+-- Horizontal
+vim.keymap.set("n", "<leader>s", "<C-w>5-", { desc = "Decrease split height"})
+vim.keymap.set("n", "<leader>t", "<C-w>5+", { desc = "Increase split height"})

c69630bb4715948794fc24075375c8a51600f8a4
Saad Bash <saad.bash@cvshealth.com>
2024-05-19 21:22:06 -0400

    add new theme: night owl

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
 	-- Theme inspired by Atom
-	"rose-pine/neovim",
+	-- "rose-pine/neovim",
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("rose-pine")
+	-- end,
+	"oxfist/night-owl.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("rose-pine")
+		require("night-owl").setup()
+		vim.cmd.colorscheme("night-owl")
 	end,
 }

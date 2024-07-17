9965357fcc3b793f10848e4662e640757035d83c
Saad Bash <saad.bash@cvshealth.com>
2024-07-17 11:12:04 -0400

    update auto-format and which-key opts

--- lua/plugins/auto-format.lua
+++ lua/plugins/auto-format.lua
@@ -3,33 +3,38 @@ return {
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 	config = function()
-	local conform = require("conform")
+		local conform = require("conform")
 
-	conform.setup({
-		formatters_by_ft = {
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			lua = { "stylua" },
-			python = { "isort", "black" },
-		},
-		format_on_save = {
-			lsp_fallback = true,
-			async = false,
-			quiet = false,
-			timeout_ms = 1000,
-		},
-	})
-
-	vim.keymap.set({ "n", "v" }, "<leader>ff", function()
-		conform.format({
-			lsp_fallback = true,
-			async = false,
-			quiet = false,
-			timeout_ms = 3000,
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				quiet = false,
+				timeout_ms = 1000,
+			},
+			formatters = {
+				prettier = {
+					require_cwd = true,
+				},
+			},
 		})
-	end, { desc = "Format file or range (in visual mode)" })
+
+		vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				quiet = false,
+				timeout_ms = 3000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }

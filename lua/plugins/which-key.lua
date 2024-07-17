7ebbd840153a0821fcbaf787bd540897c8bcf37d
Saad Bash <saad.bash@cvshealth.com>
2024-07-17 11:12:04 -0400

    update auto-format and which-key opts

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,14 +1,8 @@
 return {
 	"folke/which-key.nvim",
-	event = "VimEnter",
-	config = function()
-		-- document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-		})
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
 	end,
 }

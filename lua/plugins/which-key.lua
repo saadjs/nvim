d44f14925350900a8d67866d48432aafbe706e6c
Saad Bash <skbash89@gmail.com>
2026-01-19 20:30:04 -0500

    feat: configure which-key groups
    Add group labels for Search, Harpoon, Window, Diagnostics, LSP

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,4 +5,13 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
 	end,
+	opts = {
+		spec = {
+			{ "<leader>s", group = "Search/Split" },
+			{ "<leader>h", group = "Harpoon" },
+			{ "<leader>w", group = "Window" },
+			{ "<leader>x", group = "Diagnostics" },
+			{ "gr", group = "LSP" },
+		},
+	},
 }

38ca15c300a30790586ec533301b75427f5e6451
Saad Bash <skbash89@gmail.com>
2026-01-19 20:28:57 -0500

    refactor: use numbered harpoon keybindings
    Change from <leader>hh/jj/kk/ll to <leader>1/2/3/4
    with proper descriptions

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,20 +4,12 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>ha", mark.add_file)
-		vim.keymap.set("n", "<leader>hd", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Harpoon add file" })
+		vim.keymap.set("n", "<leader>hd", ui.toggle_quick_menu, { desc = "Harpoon menu" })
 
-		vim.keymap.set("n", "<leader>hh", function()
-			ui.nav_file(1)
-		end)
-		vim.keymap.set("n", "<leader>jj", function()
-			ui.nav_file(2)
-		end)
-		vim.keymap.set("n", "<leader>kk", function()
-			ui.nav_file(3)
-		end)
-		vim.keymap.set("n", "<leader>ll", function()
-			ui.nav_file(4)
-		end)
+		vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
+		vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
+		vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
+		vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
 	end,
 }
